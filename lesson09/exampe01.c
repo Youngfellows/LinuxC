191822390ea137597fec65755484deac1d1fdaea
Youngfellows <hk20130120515@163.com>
2018-01-31 09:15:11 -0800

    比较字符串

--- lesson09/exampe01.c
+++ lesson09/exampe01.c
@@ -1,9 +1,42 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#define LEN 128//缓冲区大小
 
 int main()
 {
 	//比较字符串
+	//char array1[] = "Linux c is very good!";
+	//char array2[] = "linux c is very good!";
+	
+	char array1[LEN],array2[LEN];
+	char *p1,*p2;//char *型变量p1,p2
+	p1 = array1;//p1指向字符串array1
+	p2 = array2;//p2指向字符串array2	
+	int result;
+
+	printf("请输入两个字符串array1和array2:\n");
+	fgets(p1,LEN,stdin);//输入字符串array1
+	fgets(p2,LEN,stdin);//输入字符串array2
+
+	//比较字符串
+	//printf("%s\n%s\n",array1,array2);
+	fputs(p1,stdout);//输出字符串array1
+	fputs(p2,stdout);//输出字符串array2
+	
+	//result = strcmp(array1,array2);	
+	result = strcmp(p1,p2);	
+	if(result == 0)
+	{
+		printf("array1与array2相等!\n");
+	}else if(result > 0)
+	{
+		printf("array1大于array2\n");
+	}else
+	{
+		printf("array1小于array2\n");
+	}
+
+
 	return 0;
 }
---
+++ lesson09/exampe03.c
@@ -0,0 +1,39 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+#define SIZE 128//缓冲区大小
+#define N 5//比较前5个字符
+
+int main()
+{
+	//比较字符串(忽略大小写)
+	char array1[SIZE],array2[SIZE];//字符串array1,array2
+	char *p1,*p2;//char *指针变量p1,p2
+	int result;//比较结果
+	p1 = array1;//p1指向字符串array1
+	p2 = array2;//p2指向字符串array2
+	
+	printf("请输入字符串array1和array2:\n");
+	fgets(p1,SIZE,stdin);//输入字符串array1
+	fgets(p2,SIZE,stdin);//输入字符串array2
+	
+	fputs(p1,stdout);//输出字符串array1
+	fputs(p2,stdout);//输出字符串array2
+
+	//result = strncmp(p1,p2,N);//比较前5个字符
+	//result = strcasecmp(p1,p2);//(忽略大小写)
+	result = strncasecmp(p1,p2,N);//(忽略大小写,比较前5个字符)
+
+	if(result == 0)
+	{
+		printf("array1 与 array2相等!\n");	
+	}else if(result > 0)
+	{
+		printf("array1 大于 array2\n");	
+	}else
+	{
+		printf("array1 小于 array2\n");
+	}
+
+	return 0;
+}
