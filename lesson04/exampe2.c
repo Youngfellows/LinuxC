c292b066b0fb9185e6e78c286770dbbcd5ae8bc9
Youngfellows <hk20130120515@163.com>
2019-07-06 20:42:36 -0700

    移动指针

--- lesson04/exampe2.c
+++ lesson04/exampe2.c
@@ -4,12 +4,14 @@ int main()
 {
 	//指针的运算
 	int a = 37;
+	int b = 234;
 	int *pm;//指针变量pm
 	pm = &a;//指针变量pm指向变量a的地址
-	printf("指针变量pm的地址: pm = %p ,变量a的地址: &a = %p\n",pm,&a);
+	printf("指针变量pm的地址: pm = %p ,变量a的地址: &a = %p,a的值: a = %d,a = %d\n",pm,&a,a,*pm);
 	
-	pm ++;//指针向下移动一位(4个字节)	
-	printf("指针变量pm的地址: pm = %p ,变量a的地址: &a = %p\n",pm,&a);
+	pm ++;//指针向下移动一位(4个字节)
+	pm = &b;	
+	printf("指针变量pm的地址: pm = %p ,变量a的地址: &a = %p,a的值: a = %d,b = %d\n",pm,&a,a,*pm);
 	
 	return 0;
 }
